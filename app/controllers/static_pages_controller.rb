46b248fc6f6c60d7004ce592366b9492e47808bc
Nicolas Montes <nicolas.montes.perso@gmail.com>
2018-03-01 17:44:33 +0100

    update

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,7 @@ class StaticPagesController < ApplicationController
   
   def notam
     system('export DISPLAY=:0 && ruby /home/nmontes/Code/notam/notam.rb')
+    sleep 2
     redirect_to '/Notam.pdf'
   end
   
